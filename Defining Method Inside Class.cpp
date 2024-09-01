556baeee16c9e947b89149eb4bbe3f234a2bbfe7
alok-003 <alokchawat@gmail.com>
2024-09-01 12:56:50 +0530

    Add files via upload

---
+++ Defining Method Inside Class.cpp
@@ -0,0 +1,25 @@
+//Name:Alok Chawat
+//PRN:23070123016
+//Aim:Implemention of Classes and Objects to Define Method inside Class
+
+#include<iostream>
+using namespace std;
+
+class Student               
+{
+    public:
+    void myMethod()
+    {
+        cout<<"I am studying in 2nd year";
+    }
+};
+
+int main()
+{
+    Student s1;
+    s1.myMethod();
+}
+
+
+/*Output:
+I am studying in 2nd year*/
